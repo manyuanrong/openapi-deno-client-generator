64ba93ea1174e2cb2baa9ecc839ad41ab42a5ebc
manyuanrong <416828041@qq.com>
2024-11-10 22:03:18 +0800

    feat: koa middleware

--- koa.ts
+++ koa.ts
@@ -1,11 +1,15 @@
-// @deno-types="npm:@types/koa@^2.15.0"
-import type Koa from "npm:koa@^2.15.0";
+import type { Context, Middleware, Next } from "npm:@types/koa@^2.15.0";
 import { generate } from "./generate.ts";
 
-export function useKoaApiClient(spec: any): Koa.Middleware {
-  return (ctx: Koa.Context) => {
-    ctx.body = generate(spec);
-    ctx.status = 200;
-    ctx.type = "application/typescript";
+export function useKoaApiClient(path: string, spec: any): Middleware {
+  return (ctx: Context, next: Next) => {
+    if (ctx.path.toLowerCase().startsWith(path.toLowerCase())) {
+      ctx.status = 200;
+      ctx.type = "application/typescript";
+      ctx.body = generate(spec);
+      return true;
+    }
+
+    return next();
   };
 }
